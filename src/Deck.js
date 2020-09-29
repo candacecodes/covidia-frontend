bc38c1e52c7b898824dc00a4cb2c104fad79ef61
candacecodes <candacehchoe@gmail.com>
2020-09-29 16:31:22 -0700

    edited add card form

--- src/Deck.js
+++ src/Deck.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 // make this a filter component instead
-
+// Filter by deck description
 class Deck extends React.Component {
   render() {
     return (
       <div className="ui cards">
         <h1>Filter </h1>
+
         {/* {this.props.pets.map((pet) => (
               <Pet key={pet.id} pet={pet} onAdoptPet={this.props.onAdoptPet} />
             ))} */}
